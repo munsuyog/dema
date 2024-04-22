5f657b06850ac4560ef02c7b84bf912e3216d587
munsuyog <mundhesuyog@gmail.com>
2024-04-22 19:33:53 +0530

    Integrate io in strapi

--- dema-cms/config/plugins.js
+++ dema-cms/config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+	// ...
+	io: {
+		enabled: true,
+		config: {
+			// This will listen for all supported events on the article content type
+            contentTypes: ['api::Blog.comments', 'api::Blog.reactions', 'api::Blog.reactions.comments'],
+		},
+	},
+	// ...
+});
