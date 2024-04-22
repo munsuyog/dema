2bf628e2d7572251a8fd5ad0f58a55ae77f828a3
munsuyog <mundhesuyog@gmail.com>
2024-04-22 22:49:34 +0530

    Test Websocket

--- dema-cms/src/index.js
+++ dema-cms/src/index.js
@@ -16,5 +16,18 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    let {Server} = require('socket.io')
+    let io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: "http://154.53.59.178:30002",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["http://154.53.59.178:30003", "http://localhost:3000"],
+        credentials: true
+      }
+    })
+    io.on("connection", (socket) => {
+      console.log("User connected", socket.id)
+    })
+  },
 };
