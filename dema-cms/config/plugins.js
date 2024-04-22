0c5fd482e85c14730d09d48ade7d6f765b991102
munsuyog <mundhesuyog@gmail.com>
2024-04-22 23:49:21 +0530

    Test

--- dema-cms/config/plugins.js
+++ dema-cms/config/plugins.js
@@ -1,28 +1,50 @@
 module.exports = ({ env }) => ({
-	// ...
-	io: {
-		enabled: true,
-		config: {
-			// This will listen for all supported events on the article content type
+    // ...
+    io: {
+        enabled: true,
+        config: {
             contentTypes: [{ uid: 'api:blog.blog', actions: ['create', 'update', 'delete'] }],
-              "events":[
-          {
-            "name": "connection",
-            "handler": ({ strapi }, socket) => {
-              strapi.log.info(`[io] new connection with id ${socket.id}`);
-              socket.on('clientPing', ()=>{
-                console.log("clientPing");
-                setTimeout(()=> {
-                  socket.emit('serverPing')}, 60000);
-              })
-              
-              socket.on('join', function(room) {
-                  socket.join(room);
-              });
-            },
-          },
-        ]
-		},
-	},
-	// ...
+            events: [
+                {
+                    name: 'connection',
+                    handler: ({ strapi }, socket) => {
+                        strapi.log.info(`[io] new connection with id ${socket.id}`);
+                        
+                        socket.on('clientPing', () => {
+                            console.log("clientPing");
+                            setTimeout(() => {
+                                socket.emit('serverPing');
+                            }, 60000);
+                        });
+
+                        socket.on('join', function(room) {
+                            socket.join(room);
+                        });
+                    },
+                },
+                {
+                    name: 'create', // Listen for the create event
+                    handler: async ({ strapi }, data) => {
+                        // Emit the created data to the 'create' room
+                        strapi.io.to('create').emit('create', data);
+                    },
+                },
+                {
+                    name: 'update', // Listen for the update event
+                    handler: async ({ strapi }, data) => {
+                        // Emit the updated data to the 'update' room
+                        strapi.io.to('update').emit('update', data);
+                    },
+                },
+                {
+                    name: 'delete', // Listen for the delete event
+                    handler: async ({ strapi }, data) => {
+                        // Emit the deleted data to the 'delete' room
+                        strapi.io.to('delete').emit('delete', data);
+                    },
+                },
+            ],
+        },
+    },
+    // ...
 });
