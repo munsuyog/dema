243b7f793316b100dfcd4e73c3a7d122143bbadf
munsuyog <mundhesuyog@gmail.com>
2024-04-22 22:58:44 +0530

    Revert

--- dema-cms/config/plugins.js
+++ dema-cms/config/plugins.js
@@ -1,1 +1,28 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+	// ...
+	io: {
+		enabled: true,
+		config: {
+			// This will listen for all supported events on the article content type
+            contentTypes: ['api::blog'],
+              "events":[
+          {
+            "name": "connection",
+            "handler": ({ strapi }, socket) => {
+              strapi.log.info(`[io] new connection with id ${socket.id}`);
+              socket.on('clientPing', ()=>{
+                console.log("clientPing");
+                setTimeout(()=> {
+                  socket.emit('serverPing')}, 60000);
+              })
+              
+              socket.on('join', function(room) {
+                  socket.join(room);
+              });
+            },
+          },
+        ]
+		},
+	},
+	// ...
+});
